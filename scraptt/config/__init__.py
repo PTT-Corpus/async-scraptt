5e8495837074b27058db930eedd52fd82638d0fc
Retr0327 <retr0327.dev@gmail.com>
2024-04-08 20:25:09 +0800

    feature: export PTT domains constant (ref #2)

--- scraptt/config/__init__.py
+++ scraptt/config/__init__.py
@@ -3,6 +3,7 @@ from .base import (
     COOKIES,
     DATA_DIR,
     PTT_BASE_URL,
+    PTT_DOMAINS,
 )
 
-__all__ = ["BASE_DIR", "COOKIES", "DATA_DIR", "PTT_BASE_URL"]
+__all__ = ["BASE_DIR", "COOKIES", "DATA_DIR", "PTT_BASE_URL", "PTT_DOMAINS"]
