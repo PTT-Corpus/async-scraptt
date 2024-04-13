ea131382719d838afe9f12be2c5ed6b8a4a36073
Retr0327 <retr0327.dev@gmail.com>
2024-04-13 18:41:30 +0800

    refactor: refactor Ckip pipeline to utilize asynchronous NLP processing (ref #2)

--- scraptt/pipelines/ckip.py
+++ scraptt/pipelines/ckip.py
@@ -1,24 +1,26 @@
-from typing import (
-    Any,
-    Dict,
-)
+from __future__ import annotations
 
-from ckip2tei import generate_tei_xml
+from typing import TYPE_CHECKING
 
-from ..spiders import PttSpider
+from ckip2tei import generate_tei_xml_async
+
 from .utils import make_file_path
+
+if TYPE_CHECKING:
+    from scraptt.interfaces import Post
+    from scraptt.spiders import PttSpider
 
 
 class CkipPipeline:
     """
     The CkipPipeline object implements CKIP Chinese NLP tools on the scraped item,
-    and writes the result to xml.
+    and writes the result to XML.
     """
 
-    def process_item(self, item: Dict[str, Any], spider: PttSpider) -> None:
-        file_path = make_file_path(item, spider.data_dir)
+    async def process_item(self, item: Post, spider: PttSpider) -> None:
+        file_path = make_file_path(item, spider._arguments.data_dir)
         item["board"] = f"{item['board']}-ptt"
-        tei_xml = generate_tei_xml(item, "ptt")
+        tei_xml = await generate_tei_xml_async(item, "ptt")
 
         with open(f"{file_path}.xml", "w", encoding="utf-8") as file:
             file.write(tei_xml)
