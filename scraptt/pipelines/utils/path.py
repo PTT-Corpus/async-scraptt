4846a79e83766efc4c25198fdea5f7cd08b3d414
Retr0327 <retr0327.dev@gmail.com>
2024-04-13 15:56:10 +0800

    refactor: update import statements to include type checking (ref #2)

--- scraptt/pipelines/utils/path.py
+++ scraptt/pipelines/utils/path.py
@@ -1,26 +1,18 @@
+from __future__ import annotations
+
 from datetime import datetime
 from pathlib import Path
-from typing import (
-    Any,
-    Dict,
-)
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from scraptt.interfaces import Post
 
 
-def make_file_path(item: Dict[str, Any], data_dir: str) -> str:
-    """The make_file_path function makes the file path.
-
-    Args:
-        item (dict): the extracted data
-        data_dir (str): the data directory
-    Returns:
-        a str
-    """
-
+def make_file_path(item: Post, data_dir: str) -> str:
     board = item["board"]
-    date = datetime.fromtimestamp(int(item["date"]))
+    date = datetime.fromtimestamp(item["created_at"])
     formatted_date = date.strftime("%Y%m%d_%H%M")
-    year = date.year
-    dir_path = f"{data_dir}/{board}/{year}"
-    file_path = f"{dir_path}/{formatted_date}_{item['post_id']}"
+    dir_path = f"{data_dir}/{board}/{date.year}"
+    file_path = f"{dir_path}/{formatted_date}_{item['id']}"
     Path(dir_path).mkdir(parents=True, exist_ok=True)
     return file_path
