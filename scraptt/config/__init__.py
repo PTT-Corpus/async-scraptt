7c6cb5a385148f6ccaf666f174d44082a4ba3800
Retr0327 <retr0327.dev@gmail.com>
2024-04-13 15:53:02 +0800

    feature: export failed URLs file path (ref #2)

--- scraptt/config/__init__.py
+++ scraptt/config/__init__.py
@@ -2,8 +2,16 @@ from .base import (
     BASE_DIR,
     COOKIES,
     DATA_DIR,
+    FAILED_URLS_FILE,
     PTT_BASE_URL,
     PTT_DOMAINS,
 )
 
-__all__ = ["BASE_DIR", "COOKIES", "DATA_DIR", "PTT_BASE_URL", "PTT_DOMAINS"]
+__all__ = [
+    "BASE_DIR",
+    "COOKIES",
+    "DATA_DIR",
+    "FAILED_URLS_FILE",
+    "PTT_BASE_URL",
+    "PTT_DOMAINS",
+]
