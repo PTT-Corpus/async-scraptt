41337fbd6ea12a480bfe23ef25afca74f75b77dd
Retr0327 <retr0327.dev@gmail.com>
2024-04-29 15:51:16 +0800

    feat: implement posts fetching service (ref #2)

---
+++ scraptt/services/latest_index_fetch.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import re
+from types import FunctionType
+from typing import TYPE_CHECKING
+
+from scrapy import Request
+
+from scraptt.config import COOKIES
+
+if TYPE_CHECKING:
+    from scrapy.http.response.html import HtmlResponse
+
+
+class LatestIndexFetchService:
+    def fetch(self, parse: FunctionType):
+        def wrapper(response: HtmlResponse):
+            prev_url = response.css('.btn.wide:contains("上頁")::attr(href)').get()
+            latest_index = int(re.search(r"index(\d{1,6})\.html", prev_url).group(1))
+            board = re.search(
+                r"www\.ptt\.cc\/bbs\/([\w\d\-_]{1,30})\/", response.url
+            ).group(1)
+
+            for index in range(1, latest_index + 1):
+                url = f"https://www.ptt.cc/bbs/{board}/index{index}.html"
+                yield Request(url, cookies=COOKIES, callback=parse)
+
+        return wrapper
